af874327e883b872d38fcb7ed5c24195c384369b
Riinger <Riinger@users.noreply.github.com>
2025-11-04 19:37:49 +0000

    Initial version

---
+++ src/test/java/com/gade/strava/strava_wrapper/StravaWrapperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gade.strava.strava_wrapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StravaWrapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
